e8055b6df32864043d11ae3bf492ec31dc7073c0
João Angello <joaoangello10@gmail.com>
2023-04-03 13:56:41 -0400

    Up cloneFolder app

---
+++ cloneFolder/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string sourceDirectory = @"C:\Users\joao.oliveira\Downloads\Nova pasta"; 
+        string targetDirectory = @"C:\Users\joao.oliveira\Downloads\en-US";
+
+        Copy(sourceDirectory, targetDirectory);
+    }
+
+    private static void Copy(string sourceDirectory, string targetDirectory)
+    {
+        DirectoryInfo diSource = new DirectoryInfo(sourceDirectory);
+        DirectoryInfo diTarget = new DirectoryInfo(targetDirectory);
+
+        CopyAll(diSource, diTarget);
+    }
+
+    private static void CopyAll(DirectoryInfo source, DirectoryInfo target)
+    {
+        foreach (DirectoryInfo dir in source.GetDirectories())
+        {
+            DirectoryInfo nextTargetSubDir =
+            target.CreateSubdirectory(dir.Name);
+            CopyAll(dir, nextTargetSubDir);
+        }
+    }
+}
